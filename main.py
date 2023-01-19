8911cee3e5a2856d4e5e7c080148598596e15fbf
JacobJEdwards <jacob.john1@talktalk.net>
2023-01-19 22:53:37 +0000

    formatting

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import datetime
 import logging
 from datetime import date
@@ -7,10 +6,16 @@ from typing import Optional, Tuple
 import redis
 from dateutil.relativedelta import relativedelta
 from telegram import *
-from telegram.ext import Application, ChatMemberHandler, ContextTypes, CallbackContext, CommandHandler
+from telegram.ext import (
+    Application,
+    ChatMemberHandler,
+    ContextTypes,
+    CallbackContext,
+    CommandHandler,
+)
 import os
 
-BOT_TOKEN = '5758011022:AAH7O85XpgCfhVsWRPQ6r4RGlgRkmQZIjmA'
+BOT_TOKEN = "5758011022:AAH7O85XpgCfhVsWRPQ6r4RGlgRkmQZIjmA"
 
 # enable logging
 logging.basicConfig(
@@ -21,13 +26,17 @@ logger = logging.getLogger(__name__)
 
 r = redis.Redis()
 
-nic_id = '5748600445'
+nic_id = "5748600445"
 
 
 # allows the bot to know if a member update is a join or leave
-def getStatusChange(chat_member_update: ChatMemberUpdated) -> Optional[Tuple[bool, bool]]:
+def getStatusChange(
+    chat_member_update: ChatMemberUpdated,
+) -> Optional[Tuple[bool, bool]]:
     status_change = chat_member_update.difference().get("status")
-    old_is_member, new_is_member = chat_member_update.difference().get("is_member", (None, None))
+    old_is_member, new_is_member = chat_member_update.difference().get(
+        "is_member", (None, None)
+    )
 
     if status_change is None:
         return None
@@ -36,12 +45,12 @@ def getStatusChange(chat_member_update: ChatMemberUpdated) -> Optional[Tuple[boo
     was_member = old_status in [
         ChatMember.MEMBER,
         ChatMember.OWNER,
-        ChatMember.ADMINISTRATOR
+        ChatMember.ADMINISTRATOR,
     ] or (old_status == ChatMember.RESTRICTED and old_is_member is True)
     is_member = new_status in [
         ChatMember.MEMBER,
         ChatMember.OWNER,
-        ChatMember.ADMINISTRATOR
+        ChatMember.ADMINISTRATOR,
     ] or (new_status == ChatMember.RESTRICTED and new_is_member is True)
 
     return was_member, is_member
@@ -55,12 +64,16 @@ async def memberStatusChange(update: Update, context: CallbackContext) -> None:
         return
     was_member, is_member = result
 
-    subscription_start = date.today().strftime('%d/%m/%Y')
-    subscription_end = (date.today() + relativedelta(months=+1)).strftime('%d/%m/%Y')
+    subscription_start = date.today().strftime("%d/%m/%Y")
+    subscription_end = (date.today() + relativedelta(months=+1)).strftime("%d/%m/%Y")
 
     memberID = update.chat_member.new_chat_member.user.id
     chatID = update.effective_chat.id
-    username = update.chat_member.new_chat_member.user.first_name+' '+update.chat_member.new_chat_member.user.last_name
+    username = (
+        update.chat_member.new_chat_member.user.first_name
+        + " "
+        + update.chat_member.new_chat_member.user.last_name
+    )
 
     if not was_member and is_member:
         r.rpush(memberID, subscription_start, subscription_end, chatID, username)
@@ -95,23 +108,28 @@ async def kickUser(context: ContextTypes.DEFAULT_TYPE, userid, chat_id) -> None:
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text('Use /check followed by a user id to view the users subscription')
+    await update.message.reply_text(
+        "Use /check followed by a user id to view the users subscription"
+    )
 
 
 async def manualCheck(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     try:
         userID = context.args[0]
     except IndexError:
-        await update.message.reply_text('Please use this command followed by a user id!\nFor example:\n*/check'
-                                        ' 6043385959*', parse_mode='Markdown')
+        await update.message.reply_text(
+            "Please use this command followed by a user id!\nFor example:\n*/check"
+            " 6043385959*",
+            parse_mode="Markdown",
+        )
         return
 
     if not userID.isnumeric():
-        await update.message.reply_text('Invalid User ID')
+        await update.message.reply_text("Invalid User ID")
         return
 
     if not r.exists(userID):
-        await update.message.reply_text('User is not a member!')
+        await update.message.reply_text("User is not a member!")
         return
 
     subscription_end = r.lindex(userID, 1).decode()
@@ -119,17 +137,20 @@ async def manualCheck(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
     chatID = r.lindex(userID, 2).decode()
     username = r.lindex(userID, 3).decode()
 
-    await update.message.reply_text(f'*User ID{userID}*\n*Username: *{username}\n*Chat: *{chatID}\n*Subscription start:'
-                                    f' *{subscription_start}'f'\n*Subscription end: *{subscription_end}'
-                                    , parse_mode='Markdown')
+    await update.message.reply_text(
+        f"*User ID{userID}*\n*Username: *{username}\n*Chat: *{chatID}\n*Subscription start:"
+        f" *{subscription_start}"
+        f"\n*Subscription end: *{subscription_end}",
+        parse_mode="Markdown",
+    )
 
 
 async def dailyCheck(context: CallbackContext) -> None:
     today = date.today().strftime("%d-%m-%Y")
 
-    filename = f'{today}.txt'
+    filename = f"{today}.txt"
 
-    with open(filename, 'w') as file:
+    with open(filename, "w") as file:
         for key in r.scan_iter():
             user_id = key.decode()
             subscription_start = r.lindex(key, 0).decode()
@@ -137,10 +158,12 @@ async def dailyCheck(context: CallbackContext) -> None:
             chat_id = r.lindex(key, 2).decode()
             username = r.lindex(key, 3).decode()
 
-            file.write(f'{username}: {user_id}\nChat ID: {chat_id}\nSubscription start: {subscription_start}\n'
-                       f'Subscription end: {subscription_end}\n\n')
+            file.write(
+                f"{username}: {user_id}\nChat ID: {chat_id}\nSubscription start: {subscription_start}\n"
+                f"Subscription end: {subscription_end}\n\n"
+            )
 
-    await context.bot.send_document(document=open(filename, 'rb'), chat_id=nic_id)
+    await context.bot.send_document(document=open(filename, "rb"), chat_id=nic_id)
 
     if os.path.exists(filename):
         os.remove(filename)
@@ -154,19 +177,23 @@ def main() -> None:
     job_queue = application.job_queue
 
     # runs daily
-    check_subscription = job_queue.run_daily(checkSubscriptions, time=datetime.time(hour=8))
+    check_subscription = job_queue.run_daily(
+        checkSubscriptions, time=datetime.time(hour=8)
+    )
     daily_message = job_queue.run_daily(dailyCheck, time=datetime.time(hour=7))
 
     # called when new user has been added
     # Handle members joining/leaving chats.
-    application.add_handler(ChatMemberHandler(memberStatusChange, ChatMemberHandler.CHAT_MEMBER))
+    application.add_handler(
+        ChatMemberHandler(memberStatusChange, ChatMemberHandler.CHAT_MEMBER)
+    )
 
     # command handlers
-    application.add_handler(CommandHandler('start', start))
-    application.add_handler(CommandHandler('check', manualCheck))
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("check", manualCheck))
 
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
